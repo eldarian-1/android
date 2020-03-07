7ab2b30e640d63d896f35be18dd69c779f72f970
101 <101@ctt.perm.ru>
2020-03-07 19:05:55 +0500

    Alexander seed

--- app/src/main/java/com/eldarian/translator/api/TranslationAPI.java
+++ app/src/main/java/com/eldarian/translator/api/TranslationAPI.java
@@ -2,6 +2,7 @@ package com.eldarian.translator.api;
 
 import java.util.Map;
 
+import io.reactivex.Observable;
 import retrofit2.Callback;
 import retrofit2.http.GET;
 import retrofit2.http.QueryMap;
@@ -9,6 +10,6 @@ import retrofit2.http.QueryMap;
 public interface TranslationAPI {
 
     @GET()
-    void getTranslate(@QueryMap Map<String, String> params, Callback<String> callback);
+    Observable<Translation> getTranslate(@QueryMap Map<String, String> params, Callback<String> callback);
 
 }
